a3c172bbefb084781f139dbb79f4c156ea45ef7a
John Rocha <john.rocha5@gmail.com>
2017-03-25 14:15:02 -0400

    Root window set

--- src/Controller.java
+++ src/Controller.java
@@ -1,15 +1,25 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Controller extends Application {
 
+	Scene scene;
+	Pane root;
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage windowStage) throws Exception {
+		root = new AnchorPane();
 		
+		scene = new Scene(root, 1200, 800);	// Hard coded temporarily
+		windowStage.setScene(scene);
+		windowStage.setTitle("Safety Assurance Case Editor");
+		windowStage.show();
 	}
 }
